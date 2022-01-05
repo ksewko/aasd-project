140f5b1a689f0239bd930753852f14a779f317ce
Gabriela Ossowska <gab.ossowska@gmail.com>
2022-01-05 15:59:26 +0100

    wysyłanie wiadomości z pomiarem

--- sensors.py
+++ sensors.py
@@ -2,23 +2,42 @@ import time
 import asyncio
 import random
 from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour
+from spade.behaviour import CyclicBehaviour, OneShotBehaviour
+from spade.message import Message
 
 class Sensors(Agent):
-    class SendMeasurements(CyclicBehaviour):
+
+    class SendTempToWindows(CyclicBehaviour):
         async def on_start(self):
             print("Starting behaviour [sensors]. . .")
             self.temp = 0
 
         async def run(self):
-            print("Temperature: {}".format(self.temp))
+            
             self.temp = random.randint(18, 22)
+            print("Temperature: {}".format(self.temp))
+
+            msg = Message(to="windows@localhost")       # jid odbiorcy
+            msg.set_metadata("msg_type", "INF")         # metadata wiadomości (jak w dokumentacji)
+            msg.set_metadata("sensor_id", "01")  
+            msg.set_metadata("sensor_type", "TERM")       
+            msg.body = {"value": self.temp}             # pomiar 
+
+            await self.send(msg)
+            print("Message sent!")
+
+            # set exit_code for the behaviour
+            self.exit_code = "Job Finished!"
+
+            # stop agent from behaviour
+            # await self.agent.stop()
+
             await asyncio.sleep(1)
 
     async def setup(self):
         print("Agent starting [sensors]. . .")
-        sm = self.SendMeasurements()
-        self.add_behaviour(sm)
+        self.b = self.SendTempToWindows()
+        self.add_behaviour(self.b)
 
 if __name__ == "__main__":
     sensors = Sensors("sensors@localhost", "password")
@@ -26,9 +45,11 @@ if __name__ == "__main__":
     future.result()
 
     print("Wait until user interrupts with ctrl+C")
-    try:
-        while True:
+
+    while sensors.is_alive():
+        try:
             time.sleep(1)
-    except KeyboardInterrupt:
-        print("Stopping...")
-    sensors.stop()
+        except KeyboardInterrupt:
+            sensors.stop()
+            break
+    print("Agent finished with exit code: {}".format(sensors.b.exit_code))
