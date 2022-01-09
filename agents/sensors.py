e35552d6032a693ba82d8c4e928b8d7851279912
darriia <37327518+darriia@users.noreply.github.com>
2022-01-09 17:07:44 +0100

    Dodanie pomiaru Uv

--- agents/sensors.py
+++ agents/sensors.py
@@ -51,8 +51,32 @@ class SensorsAgent(Agent):
 	        # await self.agent.stop()
 
 	        await asyncio.sleep(1)
+		
+	class SendUvToBlindsAgent(CyclicBehaviour):
+	    async def on_start(self):
+	        self.temp = 0
+
+	    async def run(self):
+	        
+	        self.temp = random.randint(0, 100)	#poziom naslonecznienia w % - 0% (pełne zachmurzenie) , 100% (pełne słońce)
+
+	        msg = Message(to="blinds@localhost")       # jid odbiorcy
+	        msg.set_metadata("msg_type", "INF")         # metadata wiadomości (jak w dokumentacji)
+	        msg.set_metadata("sensor_id", "10")  
+	        msg.set_metadata("sensor_type", "UV")       
+	        msg.body = str(self.temp)                   # pomiar (musi być string)
+
+	        await self.send(msg)
+
+	        self.exit_code = "Job Finished!"
+
+	        # stop agent from behaviour 
+	        # await self.agent.stop()
+
+	        await asyncio.sleep(1)
 
     async def setup(self):
         self.b = self.SendTempToWindowsAgent()
         self.c = self.SendTempToBlindsAgent()
-        self.add_behaviour(self.b, self.c)
+	self.d = self.SendUvToBlindsAgent()
+        self.add_behaviour(self.b, self.c, self.d)
