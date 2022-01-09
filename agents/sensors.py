a73f3106d3b5ff5d020848df3f2d5231f5e844cb
darriia <37327518+darriia@users.noreply.github.com>
2022-01-09 16:36:54 +0100

    Dodanie wysylania za żaluzji

--- agents/sensors.py
+++ agents/sensors.py
@@ -29,6 +29,30 @@ class SensorsAgent(Agent):
 
             await asyncio.sleep(1)
 
+    class SendTempToBlindsAgent(CyclicBehaviour):
+	    async def on_start(self):
+	        self.temp = 0
+
+	    async def run(self):
+	        
+	        self.temp = random.randint(18, 22)
+
+	        msg = Message(to="blinds@localhost")       # jid odbiorcy
+	        msg.set_metadata("msg_type", "INF")         # metadata wiadomości (jak w dokumentacji)
+	        msg.set_metadata("sensor_id", "01")  
+	        msg.set_metadata("sensor_type", "TERM")       
+	        msg.body = str(self.temp)                   # pomiar (musi być string)
+
+	        await self.send(msg)
+
+	        self.exit_code = "Job Finished!"
+
+	        # stop agent from behaviour 
+	        # await self.agent.stop()
+
+	        await asyncio.sleep(1)
+
     async def setup(self):
         self.b = self.SendTempToWindowsAgent()
-        self.add_behaviour(self.b)
+        self.c = self.SendTempToBlindsAgent()
+        self.add_behaviour(self.b, self.c)
